ebca277c281e663281f952ac07c5fb5bbbc6759a
jamesgrubb <jamesgrubb@mac.com>
2017-07-09 21:08:58 +0100

    including pug

--- app/app.js
+++ app/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+app.set('view engine' , 'pug')
+app.set('views', path.join(__dirname, '/views'));
 app.get('/' , function(req,res){
-	res.send('<h1>Hello</h1>')
+	res.render('index' , {
+		title: 'Hey',
+		message : 'What the fuck you slow cunt!!!'
+	})
 })
-const server = app.listen(3000, function(){
-	console.log('Listening on port 3000')
+
+app.set('port' , process.env.PORT || 3000);
+const server = app.listen(app.get('port'),function(){
+	console.log('Listening on port ' + app.get('port'))
 })
